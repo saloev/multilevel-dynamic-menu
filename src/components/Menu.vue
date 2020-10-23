dfaf0fad2cc46cc046c6e8b23b24dd8e861b4418
saloev <saloev.saadi@yandex.ru>
2020-10-23 22:45:28 +0300

    feat: add keyboard support

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -8,9 +8,10 @@ import { MenuItem } from "@/types";
 
 interface MenuItemType extends MenuItem {
   show?: boolean;
+  level?: string;
 }
 
-const renderMenu = (h: CreateElement, list: Array<MenuItemType>, level: string): VNode => {
+const renderMenu = (h: CreateElement, list: Array<MenuItemType>): VNode => {
   return h(
     "ul",
     {
@@ -34,18 +35,17 @@ const renderMenu = (h: CreateElement, list: Array<MenuItemType>, level: string):
             props: {
               item: {
                 ...item,
-                text: `${item.text} ${level ? level + "." : ""}${index + 1}`
+                text: `${item.text} ${item.level}`
               }
             },
             class: {
               "menu-list__link": true
+            },
+            attrs: {
+              "data-id": item.level
             }
           }),
-          ...[
-            item.subMenu && item.subMenu.length
-              ? renderMenu(h, item.subMenu, `${level ? level + "." : ""}${index + 1}`)
-              : []
-          ]
+          ...[item.subMenu && item.subMenu.length ? renderMenu(h, item.subMenu) : []]
         ]
       )
     )
@@ -55,39 +55,63 @@ const renderMenu = (h: CreateElement, list: Array<MenuItemType>, level: string):
 @Component({})
 export default class Menu extends Vue {
   @Prop({ type: Array, required: true }) list!: Array<MenuItem>;
-  normalizedList: Array<MenuItemType> = [];
+  //https://github.com/vuejs/vue/issues/2410#issuecomment-318487855 :(
+  setChangeTracker: number =  1; // :(
+  currentClicedLinksId: Set<string> = new Set();
 
-  mounted() {
-    this.addShowPropToList();
-  }
+  mounted() {}
 
-  addShowPropToList() {
+  get normalizedList(): Array<MenuItemType> {
+    //TODO make deep copy
     const copy = [...this.list];
-    const iter = (items: Array<MenuItemType>): Array<MenuItemType> => {
-      return items.map((item: MenuItemType) => {
-        item.show = false;
-        if (item.subMenu) item.subMenu = iter(item.subMenu);
+
+    const iter = (items: Array<MenuItemType>, level: string): Array<MenuItemType> => {
+      return items.map((item: MenuItemType, index: number) => {
+        const newlevel = `${level ? level + "." : ""}${index + 1}`;
+        item.show = this.currentClicedLinksId.has(newlevel);
+        item.level = newlevel;
+        if (item.subMenu) item.subMenu = iter(item.subMenu, newlevel);
         return item;
       });
     };
-    this.normalizedList = iter(copy);
+    
+    if (this.setChangeTracker)
+      return  iter(copy, "");
+    
+    return copy;
   }
 
-  onSpaceClick(): VoidFunction | undefined {
-    const elem: Element | null = document.activeElement;
-    const isMenuLink = elem && elem.classList.contains("menu-list__link");
-    if (!isMenuLink) return;
+  onSpaceClick(id: string) {
+    const isSubTree = () => {
+      const discardLastItem = id.split('.').slice(0, -1).join('.');
+      const subIds = discardLastItem === '.' ? id : discardLastItem;
 
-    const li: Element | null = document.parentElement;
-    if (!li) return;
+      return this.currentClicedLinksId.has(subIds);
+    }
+
+    if (!isSubTree()) this.onBackquoteClick();
+
+    this.currentClicedLinksId.add(id);
+    this.setChangeTracker += 1;
   }
 
-  onBackspaceClikc(): VoidFunction | undefined {
-    return;
+  onBackspaceClikc() {
+    function getLastValue(set: Set<string>): string | undefined {
+      let value;
+      for (value of set);
+      return value;
+    }
+
+    const lastVal = getLastValue(this.currentClicedLinksId);
+    if (lastVal) {
+      this.setChangeTracker -= 1;
+      this.currentClicedLinksId.delete(lastVal);
+    }
   }
 
-  onEnterClick(): VoidFunction | undefined {
-    return;
+  onBackquoteClick() {
+    this.currentClicedLinksId = new Set();
+    this.setChangeTracker = 1;
   }
 
   public render(h: CreateElement): VNode {
@@ -98,20 +122,25 @@ export default class Menu extends Vue {
           menu: true
         },
         on: {
-          keyup: ({ code }: KeyboardEvent) => {
+          keyup: ({ code, target }: KeyboardEvent) => {
+            if (!target) return;
+            //@ts-ignore
+            const id = target.dataset && target.dataset.id;
+            if (!id) return;
+
             const dispathByCode: {
-              [key: string]: VoidFunction | undefined;
+              [key: string]: VoidFunction;
             } = {
-              Space: this.onSpaceClick(),
-              Backspace: this.onBackspaceClikc(),
-              Enter: this.onEnterClick()
+              Space: () => this.onSpaceClick(id),
+              Backspace: () => this.onBackspaceClikc(),
+              Backquote: () => this.onBackquoteClick()
             };
 
-            dispathByCode[code];
+            if (typeof  dispathByCode[code] !== 'undefined') dispathByCode[code]();
           }
         }
       },
-      [renderMenu(h, this.list, "")]
+      [renderMenu(h, this.normalizedList)]
     );
   }
 }
@@ -120,6 +149,7 @@ export default class Menu extends Vue {
 @mixin hide() {
   opacity: 0;
   visibility: hidden;
+  pointer-events: none;
 }
 
 @mixin show() {
